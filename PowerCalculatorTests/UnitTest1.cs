eaaaf5a2277aef1b1b9d95257d1aaeb68e6ddc58
barossboti <150018760+barossboti@users.noreply.github.com>
2025-02-26 12:38:00 +0100

    Add files via upload

---
+++ PowerCalculatorTests/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Power;
+using System;
+
+namespace PowerCalculatorTests
+{
+    [TestClass]
+    public class PowerCalculatorTests
+    {
+        [TestMethod]
+        public void TestPositiveExponent()
+        {
+            //Arrange
+            var calculator = new PowerCalculator();
+            double basee = 2;
+            int exponent = 3;
+            //Act
+            double result = calculator.CalculatePower(basee, exponent);
+            //Assert
+            Assert.Equals(0, result);
+        }
+
+        [TestMethod]
+        public void TestNegativeExponent()
+        {
+            //Arrange
+            var calculator = new PowerCalculator();
+            double basee = 2;
+            int exponent = -2;
+            //Act
+            double result = calculator.CalculatePower(basee, exponent);
+            //Assert
+            Assert.Equals(0.25, result);
+        }
+        [TestMethod]
+        public void TestZeroBaseZeroExponent()
+        {
+            //Arrange
+            var calculator = new PowerCalculator();
+            double basee = 0;
+            int exponent = 0;
+            //Act
+            double result = calculator.CalculatePower(basee, exponent);
+            //Assert
+            Assert.Equals(0, result);
+        }
+        [TestMethod]
+        public void TestOneBaseAnyExponent()
+        {
+            //Arrange
+            var calculator = new PowerCalculator();
+            double basee = 1;
+            int exponent = 100;
+            //Act
+            double result = calculator.CalculatePower(basee, exponent);
+            //Assert
+            Assert.Equals(1, result);
+        }
+        [TestMethod]
+        public void TestNegativeBaseEvenExponent()
+        {
+            //Arrange
+            var calculator = new PowerCalculator();
+            double basee = (-3);
+            int exponent = 4;
+            //Act
+            double result = calculator.CalculatePower(basee, exponent);
+            //Assert
+            Assert.Equals(1, result);
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    public class PowerCalculator
+    {
+        public double CalculatePower(double basee, int exponent)
+        {
+            
+            if (exponent < 0)
+            {
+                throw new ArgumentException("A kitévő nem lehet negatív");
+            }
+
+            double result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= basee;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Add meg az első számot");
+            int basee = int.Parse(Console.ReadLine());
+
+            Console.Write("Add meg a második számot");
+            int exponent = int.Parse(Console.ReadLine());
+            var calculator = new PowerCalculator();
+            double result = calculator.CalculatePower(basee, exponent);
+
+            Console.WriteLine($"{basee} * {exponent} = {result}");
+
+        }
+    }
+}
